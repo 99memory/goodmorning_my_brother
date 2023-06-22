58ddda7a289d2800f927382251abf87abda1ab1f
99memory <102737052+99memory@users.noreply.github.com>
2023-06-22 12:07:35 +0800

    Update main.py
    修改获取天气API

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ template_id = os.environ["TEMPLATE_ID"]
 
 
 def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+  url = "http://wthrcdn.etouch.cn/weather_mini?city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
   return weather['weather'], math.floor(weather['temp'])
